b8ed7c3e0042d80d799af4f05c87031e8fdef5c4
Anaka Norfleet <fleetster22@gmail.com>
2023-08-16 16:51:18 -0400

    journal updates and pgadmin addition

--- api/migrations/001_create_accounts.py
+++ api/migrations/001_create_accounts.py
@@ -2,10 +2,10 @@ steps = [
     [
         # "Up" SQL statement
         """
-        CREATE TABLE users (
+        CREATE TABLE accounts (
             id SERIAL PRIMARY KEY NOT NULL,
-            first VARCHAR(30) NOT NULL,
-            last VARCHAR(40) NOT NULL,
+            first_name VARCHAR(30) NOT NULL,
+            last_name VARCHAR(50) NOT NULL,
             email VARCHAR(100) NOT NULL UNIQUE
         );
         """,
