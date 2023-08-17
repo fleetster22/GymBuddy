b1283ec036d84a60273d0342bf0bae8f712a6381
Samantha <samanthahartig@gmail.com>
2023-08-17 16:22:37 -0400

    workouts work started

--- api/main.py
+++ api/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
+from routers import workouts
 
 app = FastAPI()
+app.include_router(workouts.router, prefix="/api/workouts")
 
 app.add_middleware(
     CORSMiddleware,
