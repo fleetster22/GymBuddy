1b7c9386cf25f13122c102114ad821d932899593
Anaka Norfleet <fleetster22@gmail.com>
2023-08-21 17:08:17 -0400

    started exercises frontend

--- api/main.py
+++ api/main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from authenticator import authenticator
 from fastapi.middleware.cors import CORSMiddleware
 import os
-from routers import exercises, accounts
+from routers import exercises, accounts, workouts
 
 app = FastAPI()
+app.include_router(authenticator.router, prefix="/api/auth")
 app.include_router(exercises.router, prefix="/api/exercises")
-
+app.include_router(workouts.router, prefix="/api/workouts")
 app.include_router(accounts.router, prefix="/api/accounts")
 
 app.add_middleware(
