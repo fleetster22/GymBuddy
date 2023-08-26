6e17f97eb1b2722beb9fc3ef6841165ce3a2e632
Michael Parnapy <sgtparn@gmail.com>
2023-08-25 21:52:57 -0400

    Initial Sass styling

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateWorkout from "./CreateWorkout.js";
-import WorkoutPage from "./WorkoutPage.jsx";
+// import WorkoutPage from "./WorkoutPage.jsx";
+import Nav from "./Nav.js";
+import AboutUs from "./About.js";
+import MainPage from "./MainPage.js";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
+        <Nav />
+
         <Routes>
           <Route path="exercises">
             <Route index element={<CreateWorkout />} />
-            <Route path="history" element={<WorkoutPage />} />
+            {/* <Route path="history" element={<WorkoutPage />} /> */}
           </Route>
+          <Route path="/MainPage" element={<MainPage />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ ghi/src/Header.js
@@ -0,0 +1,14 @@
+import "./main.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header__text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary--main">SCULPTR</span>
+          <span className="heading-primary--sub">exercise for humans</span>
+        </h1>
+      </div>
+    </header>
+  );
+}
