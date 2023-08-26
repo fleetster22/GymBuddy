f20489c087863b7fa92b3e90e7d8d23aee2d593b
Samantha <samanthahartig@gmail.com>
2023-08-25 21:26:23 -0400

    Create a workout page with Phil's help. See journal entry.

--- ghi/src/components/CreateWorkout.js
+++ ghi/src/components/CreateWorkout.js
@@ -4,8 +4,41 @@ function ExercisesList() {
 	const [exercises, setExercises] = useState([]);
 	const [filterType, setFilterType] = useState("");
 	const [filter, setFilter] = useState("");
-	const [workout, setWorkout] = useState([]);
 	const [endpoint, setEndpoint] = useState("");
+	const [workout, setWorkout] = useState({
+		name: "",
+		description: "",
+		date: "",
+		exercises: [],
+	});
+	const handleChange = (e) => {
+		setWorkout({ ...workout, [e.target.name]: e.target.value });
+	};
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+		const body = JSON.stringify(workout);
+		console.log(body);
+		try {
+			const response = await fetch("http://localhost:8000/api/workouts/", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(workout),
+			});
+			if (response.ok) {
+				setWorkout({
+					name: "",
+					description: "",
+					date: "",
+					exercises: [],
+				});
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	}
 
 	async function fetchExercises() {
 		if (filterType && filter) {
@@ -25,7 +58,7 @@ function ExercisesList() {
 	}
 
 	const addWorkout = (entry) => {
-		setWorkout([...workout, entry]);
+		setWorkout({ ...workout, exercises: [...workout.exercises, entry] });
 	};
 
 	const updateFilter = (e) => {
@@ -42,74 +75,76 @@ function ExercisesList() {
 	}, [filter]);
 
 	return (
-		<div>
-			<div>
-				<select
-					value={filterType}
-					onChange={(e) => updateFilter(e)}
-				>
-					<option value="type">Types</option>
-					<option value="muscle">Muscle Group</option>
-					<option value="difficulty">Difficulty</option>
-				</select>
-				{filterType === "type" ? (
+		<div style={{ display: "flex" }}>
+			<div style={{ flex: 1 }}>
+				<div>
 					<select
-						value={filter}
-						onChange={(e) => setFilter(e.target.value)}
+						value={filterType}
+						onChange={(e) => updateFilter(e)}
 					>
-						<option value="all">All Types</option>
-						<option value="cardio">Cardio</option>
-						<option value="olympic_weightlifting">Olympic Weightlifting</option>
-						<option value="plyometrics">Plyometrics</option>
-						<option value="powerlifting">Powerlifting</option>
-						<option value="strength">Strength</option>
-						<option value="stretching">Stretching</option>
-						<option value="strongman">Strongman</option>
+						<option value="type">Types</option>
+						<option value="muscle">Muscle Group</option>
+						<option value="difficulty">Difficulty</option>
 					</select>
-				) : filterType === "muscle" ? (
-					<select
-						value={filter}
-						onChange={(e) => setFilter(e.target.value)}
-					>
-						<option value="all">All Muscles</option>
-						<option value="abdominals">Abdominals</option>
-						<option value="abductors">Abductors</option>
-						<option value="adductors">Adductors</option>
-						<option value="biceps">Biceps</option>
-						<option value="calves">Calves</option>
-						<option value="chest">Chest</option>
-						<option value="forearms">Forearms</option>
-						<option value="glutes">Glutes</option>
-						<option value="hamstrings">Hamstrings</option>
-						<option value="lats">Lats</option>
-						<option value="lower_back">Lower Back</option>
-						<option value="middle_back">Middle Back</option>
-						<option value="neck">Neck</option>
-						<option value="quadriceps">Quadriceps</option>
-						<option value="traps">Traps</option>
-						<option value="triceps">Triceps</option>
-					</select>
-				) : filterType === "difficulty" ? (
-					<select
-						value={filter}
-						onChange={(e) => setFilter(e.target.value)}
-					>
-						<option value="all">All Difficulties</option>
-						<option value="beginner">Beginner</option>
-						<option value="intermediate">Intermediate</option>
-						<option value="expert">Expert</option>
-					</select>
-				) : null}
-				<button onClick={() => fetchExercises()}>Search</button>
-			</div>
+					{filterType === "type" ? (
+						<select
+							value={filter}
+							onChange={(e) => setFilter(e.target.value)}
+						>
+							<option value="all">All Types</option>
+							<option value="cardio">Cardio</option>
+							<option value="olympic_weightlifting">
+								Olympic Weightlifting
+							</option>
+							<option value="plyometrics">Plyometrics</option>
+							<option value="powerlifting">Powerlifting</option>
+							<option value="strength">Strength</option>
+							<option value="stretching">Stretching</option>
+							<option value="strongman">Strongman</option>
+						</select>
+					) : filterType === "muscle" ? (
+						<select
+							value={filter}
+							onChange={(e) => setFilter(e.target.value)}
+						>
+							<option value="all">All Muscles</option>
+							<option value="abdominals">Abdominals</option>
+							<option value="abductors">Abductors</option>
+							<option value="adductors">Adductors</option>
+							<option value="biceps">Biceps</option>
+							<option value="calves">Calves</option>
+							<option value="chest">Chest</option>
+							<option value="forearms">Forearms</option>
+							<option value="glutes">Glutes</option>
+							<option value="hamstrings">Hamstrings</option>
+							<option value="lats">Lats</option>
+							<option value="lower_back">Lower Back</option>
+							<option value="middle_back">Middle Back</option>
+							<option value="neck">Neck</option>
+							<option value="quadriceps">Quadriceps</option>
+							<option value="traps">Traps</option>
+							<option value="triceps">Triceps</option>
+						</select>
+					) : filterType === "difficulty" ? (
+						<select
+							value={filter}
+							onChange={(e) => setFilter(e.target.value)}
+						>
+							<option value="all">All Difficulties</option>
+							<option value="beginner">Beginner</option>
+							<option value="intermediate">Intermediate</option>
+							<option value="expert">Expert</option>
+						</select>
+					) : null}
+					<button onClick={() => fetchExercises()}>Search</button>
+				</div>
 
-			<div>
 				{exercises?.map((exercise) => (
 					<div
 						key={exercise.name}
 						style={{ border: "black solid 1px", padding: "10px" }}
 					>
-						{workout.includes(exercise) ? (
+						{workout.exercises.includes(exercise.name) ? (
 							<h3 style={{ color: "green" }}>Added to workout</h3>
 						) : null}
 						<h3>{exercise.name}</h3>
@@ -117,14 +152,62 @@ function ExercisesList() {
 						<p>{exercise.muscle}</p>
 						<p>{exercise.equipment}</p>
 						<p>{exercise.difficulty}</p>
-						<button onClick={() => addWorkout(exercise)}>Add to workout</button>
+						<button onClick={() => addWorkout(exercise.name)}>
+							Add to workout
+						</button>
 					</div>
 				))}
 			</div>
 
-			<div>
+			<div style={{ flex: 1, border: "black solid 1px", padding: "10px" }}>
 				<h2>Workout</h2>
-				{workout.map((exercise, index) => (
+				<form onSubmit={handleSubmit}>
+					<div>
+						<label>
+							Workout Name:
+							<input
+								type="text"
+								name="name"
+								required
+								value={workout.name}
+								placeholder="Workout name"
+								onChange={handleChange}
+							/>
+						</label>
+					</div>
+					<div>
+						<label>
+							Description:
+							<input
+								type="text"
+								name="description"
+								required
+								placeholder="Workout description"
+								value={workout.description}
+								onChange={handleChange}
+							/>
+						</label>
+					</div>
+					<div>
+						<label>
+							Workout Date:
+							<input
+								type="date"
+								name="date"
+								value={workout.date}
+								onChange={handleChange}
+							/>
+						</label>
+					</div>
+					<div>
+						<input
+							type="submit"
+							value="Create Workout"
+						/>
+					</div>
+				</form>
+				<h3>Exercises in Workout</h3>
+				{workout.exercises.map((exercise, index) => (
 					<div key={index}>{exercise.name}</div>
 				))}
 			</div>
--- journals/samantha_hartig.md
+++ journals/samantha_hartig.md
@@ -8,3 +8,15 @@ There needs to be an "add to workout" button so the exercises can be added to a 
 The workout with the names of exercises and the order they are in should also be shown on the side.
 The Workouts will be stored in a database tied to the user that created the workout.
 Maybe later add ability to drag and drop to change the order. Would require { useDrag } from 'react-dnd'
+
+8/24 The first drop down is to select the filter type (hooks state 2). The second drop down (hooks state 3) populates a list based on the specific filter selected. These are used (hooks state 5) to get to the exercises router. Clicking search will pull in the list of 10 exercises. Clicking “add to workout” adds the workout to (hooks state 4) the exercise list which I will work on getting into the database tomorrow. The “add to workout” button also (line 113 in CreateWorkout.js) adds green “Added to workout” text by (line 112) seeing if the workout is included in the exercise list, if it is not (line 114) does nothing.
+The workout at the bottom of the page allows the user to see the workout as it is being built. This should move to the side of the page so it is always visible.
+
+8/25 Phil said to make the create a workout page function we'd need to change some things in the migrations 003 create exercises and in the workouts query. He offered to come walk us through it on Monday because others on the team might be using parts of the things that need to change.
+The 003_create_exercise table needs contain the same information as the exercise query. This will involve adding the information, dropping, and remigrating the table. We should have this set up before Phil comes to help because it is just set up.
+In the workouts router - line 22-26 will be removed. Where did the matching_exercises come from?
+In queries- the exercise query file needs to be filled out with at least post an exercise.
+In workouts routers- we need to create a list that is equal to what is inside the list of workouts that was posted. Create a variable ExerciseList = workouts.exercises.
+For loop over the list to make a new list for just the names of exercises. Put that list back into the workouts.exercises (so you have a list of names without the rest of the data)
+For loop over the ExerciseList and post each into exercise database.
+The info from the two for loops (id and name) will be used in the new_workout.
