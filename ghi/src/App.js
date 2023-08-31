f84a5a5f5161ed708d3fff9390022cb7ba08c4c4
Michael Parnapy <sgtparn@gmail.com>
2023-08-31 17:23:58 -0400

    Fixed App.js

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -5,24 +5,6 @@ import WorkoutPage from "./WorkoutPage.jsx";
 import Nav from "./Nav.js";
 import About from "./About.js";
 import MainPage from "./MainPage.js";
-<<<<<<< HEAD
-
-// import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/MainPage" element={<MainPage />} />
-        <Route path="/About" element={<About />} />
-        <Route path="workouts">
-          <Route index element={<WorkoutPage />} />
-          <Route path="create" element={<CreateWorkout />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-=======
 import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 import Signupform from "./Signupform.jsx";
 import Login from "./Login.jsx";
@@ -45,7 +27,6 @@ function App() {
         </Routes>
       </BrowserRouter>
     </AuthProvider>
->>>>>>> b29444d2422e38aa06d6d67a6e7a215c9dcec993
   );
 }
 
