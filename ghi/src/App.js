b29444d2422e38aa06d6d67a6e7a215c9dcec993
Priya <pxp405@case.edu>
2023-08-31 17:10:21 -0400

    Aug 31

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -6,7 +6,9 @@ import Nav from "./Nav.js";
 import AboutUs from "./About.js";
 import MainPage from "./MainPage.js";
 import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
-
+import Signupform from "./Signupform.jsx";
+import Login from "./Login.jsx";
+import Logout from "./Logout.jsx";
 function App() {
   return (
     <AuthProvider>
@@ -15,6 +17,9 @@ function App() {
           <Route path="/" element={<Nav />} />
           <Route path="/MainPage" element={<MainPage />} />
           <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/Signupform" element={<Signupform />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Logout" element={<Logout />} />
           <Route path="workouts">
             <Route index element={<WorkoutPage />} />
             <Route path="create" element={<CreateWorkout />} />
