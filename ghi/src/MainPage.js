56d942ecc99ef9cab8bda1c6b4d1c5dde7367c4e
Michael Parnapy <sgtparn@gmail.com>
2023-09-06 09:33:01 -0400

    Fixing my mistakes2

---
+++ ghi/src/MainPage.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+import Card from "./Card";
+import Header from "./Header";
+import Questions from "./Questions";
+
+function MainPage() {
+  return (
+    <div>
+      <Header />
+      <Card />
+      <Questions />
+    </div>
+  );
+}
+
+export default MainPage;
